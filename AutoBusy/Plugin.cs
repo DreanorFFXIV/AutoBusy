959ec247769c2a0c2a60683305b0fc2e8f4e5d21
Dreanor <mikeundead@googlemail.com>
2023-10-30 16:24:33 +0100

    init

---
+++ AutoBusy/Plugin.cs
@@ -0,0 +1,79 @@
+﻿using AutoBusy.Windows;
+using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+
+namespace AutoBusy
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "AutoBusy";
+        private const string CommandName = "/pbusy";
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private ICommandManager CommandManager { get; init; }
+        public Configuration Configuration { get; init; }
+        public WindowSystem WindowSystem = new("AutoBusy");
+
+        private ConfigWindow ConfigWindow { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] ICommandManager commandManager,
+            [RequiredVersion("1.0")] IClientState clientState)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+
+            ConfigWindow = new ConfigWindow(this);
+            
+            WindowSystem.AddWindow(ConfigWindow);
+
+            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "/pbusy"
+            });
+         
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+            clientState.Login += SetBusy;
+        }
+
+        private void SetBusy()
+        {
+            if (Configuration.Enabled)
+            {
+                CommandManager.ProcessCommand("/busy");
+            }
+        }
+        
+        public void Dispose()
+        {
+            this.WindowSystem.RemoveAllWindows();
+            
+            ConfigWindow.Dispose();
+            
+            this.CommandManager.RemoveHandler(CommandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            DrawConfigUI();
+        }
+
+        private void DrawUI()
+        {
+            this.WindowSystem.Draw();
+        }
+
+        public void DrawConfigUI()
+        {
+            ConfigWindow.IsOpen = true;
+        }
+    }
+}
---
+++ AutoBusy/Windows/ConfigWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+
+namespace AutoBusy.Windows;
+
+public class ConfigWindow : Window, IDisposable
+{
+    private Configuration Configuration;
+
+    public ConfigWindow(Plugin plugin) : base(
+        "AutoBusy Config",
+        ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
+        ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        this.Size = new Vector2(232, 115);
+        this.SizeCondition = ImGuiCond.Always;
+
+        this.Configuration = plugin.Configuration;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        var enabled = this.Configuration.Enabled;
+        
+        if (ImGui.Checkbox("Enabled", ref enabled))
+        {
+            this.Configuration.Enabled = !enabled;
+            this.Configuration.Save();
+        }
+    }
+}
